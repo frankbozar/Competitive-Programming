e364e90f4c342542807c9def3f04bf8e5f932710
frankbozar <frankbozar@frank-2.local>
2017-03-30 02:08:41 +0800

    unknown

---
+++ A.cpp
@@ -0,0 +1,70 @@
+#include<cstdio>
+#include<cstring>
+#include<set>
+using namespace std;
+const int N=1<<20;
+char s[N]={0}, t[N]={0};
+int a[N]={0}, n, m;
+
+bool ok(const set<int>& S)
+{
+    int p=0;
+    
+    for(int d : S)
+    {
+        if( s[d]==t[p] )
+            if( ++p>=m )
+                return true;
+    }
+    
+    return false;
+}
+
+int main()
+{
+    scanf("%s%s", s, t);
+    n=strlen(s);
+    m=strlen(t);
+    
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+        a[i]--;
+    }
+    
+    int ans=-1, p=0;
+    set<int> S(a, a+n);
+    
+    for(int l=0, r=n; l<r; )
+    {
+        int x=(l+r)>>1;
+        
+        while( x<p )
+            S.insert(a[--p]);
+        
+        while( x>p )
+            S.erase(a[p++]);
+        
+        if( !ok(S) )
+            r=x;
+        else
+        {
+            l=x+1;
+            ans=x;
+        }
+    }
+    
+    printf("%d\n", ans);
+}
+
+/*
+
+ababcba
+abb
+5 3 4 1 7 6 2
+
+bbbabb
+bb
+1 6 3 4 2 5
+
+*/
